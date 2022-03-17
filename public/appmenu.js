ef4bfad2c4f5e133fe88c7cffcd2f5cd99e66445
CJ <cjsabijon@gmail.com>
2022-03-18 00:07:28 +0800

    Optimized code

---
+++ public/appmenu.js
@@ -0,0 +1,38 @@
+const bars = document.querySelector('#bars')
+const menu = document.querySelector('#menu')
+
+bars.addEventListener('click', () => {
+    if(menu.classList.contains('hidden') && window.innerWidth < 768) {
+        menu.classList.remove('hidden')
+        menu.classList.add(
+            'flex',
+            'flex-col',
+            'items-center',
+            'bg-blue-200',
+            'dark:bg-gray-800',
+            'w-full',
+            'absolute',
+            'top-28'   
+        );
+        }
+        else {
+            menu.classList.add('hidden');
+    }
+});
+
+menu.addEventListener('click', () => {
+    menu.classList.add('hidden');
+})
+
+window.addEventListener('resize', () => {
+    menu.classList.add('hidden');
+    menu.classList.remove(
+        'flex',
+        'flex-col',
+        'text-center',
+        'bg-blue-200',
+        'w-full',
+        'absolute',
+        'top-28'   
+    );
+})
