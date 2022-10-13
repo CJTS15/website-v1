b3fbc479a3043b62077ee480cf01f91085eaf5de
CJ Sabijon <cjsabijon@gmail.com>
2022-10-13 10:10:35 +0800

    Added updates on nav menu, and hero header.

--- public/appmenu.js
+++ public/appmenu.js
@@ -1,41 +1,45 @@
-const bars = document.querySelector('#bars')
-const menu = document.querySelector('#menu')
+const switchMenu = document.querySelector('#switch_menu');
+const list = document.querySelector('#menuList');
 
-bars.addEventListener('click', () => {
-    if(menu.classList.contains('hidden') && window.innerWidth < 768) {
-        menu.classList.remove('hidden')
-        menu.classList.add(
-            'flex',
-            'flex-col',
-            'items-center',
-            'bg-blue-200',
-            'dark:bg-gray-900',
-            'w-full',
-            'absolute',
-            'top-28',
-            'z-10'
-        );
-        }
-        else {
-            menu.classList.add('hidden');
-    }
-});
+function toggleMenu () {
+    switchMenu.name === 'menu' ? 
+    (switchMenu.name = "close" ,list.classList.add('top-[80px]', 'bg-transparent', 'md:bg-transparent', 'bg-gray-200', 'dark:bg-custom-bg-dark')) 
+    :(switchMenu.name = "menu" ,list.classList.remove('top-[80px]', 'bg-transparent', 'md:bg-transparent', 'bg-gray-200', 'dark:bg-custom-bg-dark'))
+}
 
-menu.addEventListener('click', () => {
-    menu.classList.add('hidden');
-})
+// const bars = document.querySelector('#bars')
+// const menu = document.querySelector('#menu')
+// const fmenu = document.querySelector('#fmenu')
 
-window.addEventListener('resize', () => {
-    menu.classList.add('hidden');
-    menu.classList.remove(
-        'flex',
-        'flex-col',
-        'text-center',
-        'bg-blue-200',
-        'dark:bg-gray-900',
-        'w-full',
-        'absolute',
-        'top-28',
-        'z-10'
-    );
-})
+// bars.addEventListener('click', () => {
+//     if(menu.classList.contains('hidden') && window.innerWidth < 768) {
+//         menu.classList.remove('hidden')
+//         menu.classList.add(
+//             'flex',
+//             'flex-col',
+//             'w-full',
+//             'top-28',
+//             'z-20'
+//         );
+//         }
+//         else {
+//             menu.classList.add('hidden');
+//     }
+// });
+
+// menu.addEventListener('click', () => {
+//     menu.classList.add('hidden');
+// })
+
+// window.addEventListener('resize', () => {
+//     fmenu.classList.add('hidden');
+//     menu.classList.add('hidden');
+//     menu.classList.remove(
+//         'flex',
+//         'flex-col',
+//         'w-full',
+//         'absolute',
+//         'top-28',
+//         'z-20'
+//     );
+// })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
-  purge: [],
+  content: [
+    './*.html',
+    './public/*.{html,js}',
+    './src/**/*.{html,js}',
+  ],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
     extend: {
       colors: {
         "custom-purple": "#5267DF",
@@ -9,6 +19,8 @@ module.exports = {
         "custom-blue": "#242A45",
         "custom-grey": "#9194A2",
         "custom-white": "#f7f7f7",
+        "custom-bg-dark": "#272830",
+        "custom-bg-dark1": "#2b2c34",
       },
     },
     fontFamily: {
@@ -17,11 +29,6 @@ module.exports = {
     container: {
       center: true,
       padding: "1rem",
-      screens: {
-        lg: "1124px",
-        xl: "1124px",
-        "2xl": "1124px",
-      },
     },
 
   },
